33e9eaecc90dd113241220ba5840ef9c37d2e785
Jason Springer <jsnsprngr@gmail.com>
2017-03-27 11:50:33 -0400

    Add files via upload

--- pluralsight/java-fundamentals-collections/src/main/java/com/monotonic/collections/Product.java
+++ pluralsight/java-fundamentals-collections/src/main/java/com/monotonic/collections/Product.java
@@ -1,16 +1,17 @@
 package com.monotonic.collections;
 
 import java.util.Comparator;
-import java.util.Objects;
-
-import static java.util.Comparator.comparing;
 
 public class Product
 {
-
-    public static final Comparator<Product> BY_NAME = comparing(Product::getName);
-
-    public static final Comparator<Product> BY_WEIGHT = comparing(Product::getWeight);
+    public static final Comparator<Product> BY_WEIGHT
+        = new Comparator<Product>()
+    {
+        public int compare(final Product p1, final Product p2)
+        {
+            return Integer.compare(p1.getWeight(), p2.getWeight());
+        }
+    };
 
     private final String name;
     private final int weight;
@@ -38,20 +39,5 @@ public class Product
             "name='" + name + '\'' +
             ", weight=" + weight +
             '}';
-    }
-
-    public boolean equals(final Object o)
-    {
-        if (!(o instanceof Product)) return false;
-
-        final Product product = (Product) o;
-
-        return Objects.equals(weight, product.weight)
-            && Objects.equals(name, product.name);
-    }
-
-    public int hashCode()
-    {
-        return Objects.hash(name, weight);
     }
 }
